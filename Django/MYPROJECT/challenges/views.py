54fac0b4907c85fdd1c643799b5f7dbe4cfaa0ad
unknown <tayalmayank60@gmail.com>
2026-03-28 00:05:45 +0530

    cleaned

--- Django/MYPROJECT/challenges/views.py
+++ Django/MYPROJECT/challenges/views.py
@@ -4,20 +4,6 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 # Create your views here.
 
-# Static Approach
-# def index(request):
-#     return HttpResponse("This WOrks!")
-
-
-# def schedule(request):
-#     return HttpResponse("You need to go for running!!!!")
-
-
-
-
-
-
-# Dynamic Aproach
 metadata = {
     "january": "1st Month",
     "february": "2nd Month",
@@ -35,15 +21,9 @@ metadata = {
 
 
 def challenge_page(request):
-    # data = ""
+    
     month_list = list(metadata.keys())
 
-    # for month in month_list:
-    #     mod = month.capitalize()
-    #     month_path = reverse("main_url",args= [month])
-    #     data += f"<li><a href = \" {month_path}\">{mod}</a></li>"
-    # respo = f"<ul>{data}</ul>"
-    # return HttpResponse(respo)
     return render(request,"challenges/index.html",{
         "months": month_list
     })
@@ -52,10 +32,9 @@ def challenge_page(request):
 def monthly_challenges(request, month):
     try:
         text = metadata[month]
-        # responseData = render_to_string("challenges/challenge.html")
-        # return HttpResponse(responseData)
+        
 
-        # kinda like shortcut above 2 lines can be reduced to this single line
+        
         return render(request, "challenges/challenge.html",{
             "output" : text,
             "month" : month.capitalize()
@@ -65,14 +44,12 @@ def monthly_challenges(request, month):
 
 
 
-# redirection
 def redirect_months(request, month):
     months = list(metadata.keys())
     if month > len(months):
         return HttpResponse("Invalid!!!")
     redir_month = months[month - 1]
     redir_path = reverse("main_url", args=[redir_month])
-    # return HttpResponseRedirect("/challenges/" + redir_month)  #hardcoded path
     return HttpResponseRedirect(redir_path)    #dynamic path
 
     
