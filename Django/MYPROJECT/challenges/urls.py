2cbaac7eb867d161a5c37d6dcdc3287aaa4a2a3f
unknown <tayalmayank60@gmail.com>
2026-03-28 00:46:55 +0530

    include feature or partial templates

--- Django/MYPROJECT/challenges/urls.py
+++ Django/MYPROJECT/challenges/urls.py
@@ -2,14 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # static way to add urls
-    # path("january", views.index),
-    # path("febuary",views.schedule),
-
-    path("",views.challenge_page),
-    # redirection
+    
+    path("",views.challenge_page, name = "index"),
     path("<int:month>",views.redirect_months),
-
-    # dynamic ways to add url
     path("<str:month>",views.monthly_challenges, name = "main_url"),
 ]
