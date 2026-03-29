6e5441480959e127003fd8b06dd37b7e4d19cc1a
unknown <tayalmayank60@gmail.com>
2026-03-29 18:11:31 +0530

    Done making migrations and migrating schema to data base

--- Django/Book_Store/book_outlet/models.py
+++ Django/Book_Store/book_outlet/models.py
@@ -8,3 +8,4 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
     # Do not need to give ID as django automatically create ID with auto-incrementing value
+    
