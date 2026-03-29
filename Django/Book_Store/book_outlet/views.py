87f918d6349324205fb55b56107d6a85b2e83e10
unknown <tayalmayank60@gmail.com>
2026-03-29 22:49:13 +0530

    Dummy Data added to DB and render to webpage that data

--- Django/Book_Store/book_outlet/views.py
+++ Django/Book_Store/book_outlet/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def index(request):
+    all_books = Book.objects.all()
+    return render(request, "book_outlet/index.html",{
+        "books": all_books,
+    })
