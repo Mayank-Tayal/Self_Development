88b14316173156a065b7eb7927ccb425bcd6de78
unknown <tayalmayank60@gmail.com>
2026-03-29 18:36:39 +0530

    Done Entering Data, Updating Schema and Creating and upadating method for schema

--- Django/Book_Store/book_outlet/models.py
+++ Django/Book_Store/book_outlet/models.py
@@ -1,11 +1,19 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator,MaxValueValidator
 # Create your models here.
 
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxValueValidator(5)]
+        )
+    author = models.CharField(null=True, max_length=100)
+    isBestSelling = models.BooleanField(default=False)
     # Do not need to give ID as django automatically create ID with auto-incrementing value
-    
+     
+
+
+    def __str__(self):
+       return f"{self.title} ({self.rating})"
