a13ecc809f590fd97ed04b281fd7825608c94941
unknown <tayalmayank60@gmail.com>
2026-03-30 23:47:42 +0530

    Learn: Admin Interface and Composing it

--- Django/Book_Store/book_outlet/admin.py
+++ Django/Book_Store/book_outlet/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+
+    
+    # readonly_fields = ("slug",) # make mentioned field read only 
+
+
+    prepopulated_fields = {"slug":("title",)} 
+    # prepopulate the field(key in dict) with the field(value of the key)
+    """cannot use readonly_fields with prepopulated fields as it contradicts and 
+    django wil throw error for it"""
+
+
+    list_filter = ('author', 'rating')
+
+    list_display = ('title', 'author')
+
+admin.site.register(Book,BookAdmin)
--- Django/Book_Store/book_outlet/models.py
+++ Django/Book_Store/book_outlet/models.py
@@ -7,21 +7,27 @@ from django.utils.text import slugify
 
 
 class Book(models.Model):
+    # Do not need to give ID as django automatically create ID with auto-incrementing value
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinLengthValidator(1), MaxValueValidator(5)]
         )
     author = models.CharField(null=True, max_length=100)
     isBestSelling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
-    # Do not need to give ID as django automatically create ID with auto-incrementing value
-    
+    slug = models.SlugField(default="", null=False,blank=True, db_index=True)
+    # editable=False removes the field, in admin readonly allows to show but not able to edit
+    # editable=False needs to be removed for use of prepopulated in admin
+    # blank=True allows field to be empty
+
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+
+
+    # does not need this method as prepopulated_fields in admin already calls slugify
+    # while entering new entries to database 
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
        return f"{self.title} ({self.rating})"
