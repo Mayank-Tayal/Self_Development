2f1b340e0ebff56968d824d901dd9356e6909e84
unknown <tayalmayank60@gmail.com>
2026-04-02 02:58:33 +0530

    Blog: Setting Up Models and Admin Done

--- Django/my_site/blog/models.py
+++ Django/my_site/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'#{self.caption}'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    e_mail_address = models.EmailField()
+
+    def get_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.get_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=50)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="post")
+    tags = models.ManyToManyField(Tag)
+    content = models.TextField(null=True)
+    date = models.DateField(default=date.today)
+    def __str__(self):
+        return f"{self.title}"
+
