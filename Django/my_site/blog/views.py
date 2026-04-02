af6c9b7bd054cf23132b00b2d5187917bff2f2a1
unknown <tayalmayank60@gmail.com>
2026-04-02 16:20:46 +0530

    Done intergrating DataBase data to frontend

--- Django/my_site/blog/views.py
+++ Django/my_site/blog/views.py
@@ -2,82 +2,82 @@ from django.shortcuts import render
 from datetime import date
 # Create your views here.
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Mayank",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+# all_posts = [
+#     {
+#         "slug": "hike-in-the-mountains",
+#         "image": "mountains.jpg",
+#         "author": "Mayank",
+#         "date": date(2021, 7, 21),
+#         "title": "Mountain Hiking",
+#         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Mayank",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "programming-is-fun",
+#         "image": "coding.jpg",
+#         "author": "Mayank",
+#         "date": date(2022, 3, 10),
+#         "title": "Programming Is Great!",
+#         "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Mayank",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     },
+#     {
+#         "slug": "into-the-woods",
+#         "image": "woods.jpg",
+#         "author": "Mayank",
+#         "date": date(2020, 8, 5),
+#         "title": "Nature At Its Best",
+#         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+#         "content": """
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
+#           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+#           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+#           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+#         """
+#     }
+# ]
 
-def get_date(post):
-    return post["date"]
 
+from .models import Tag, Author, Post
+
+all_posts = Post.objects.all()
 
 
 def starting_page(requests):
-    sorted_posts = sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    
     return render(requests,"blog/index.html",{
-        "posts": latest_posts
+        "posts": Post.objects.order_by('-date')[:3]
     })
 
 
@@ -88,7 +88,15 @@ def posts(requests):
 
 
 def post_detail(requests, slug):
-    id_post = next(post for post in all_posts if post['slug'] == slug)
+    id_post = next(post for post in all_posts if post.slug == slug)
     return render(requests, "blog/post-detail.html",{
-        "post":id_post
+        # "post":id_post
+        'title': id_post.title,
+        'excerpt': id_post.excerpt,
+        'image': id_post.image_name,
+        'slug': id_post.slug,
+        'author': id_post.author,
+        'tags': id_post.tags.all(),
+        'content': id_post.content,
+        'date': id_post.date
     })
