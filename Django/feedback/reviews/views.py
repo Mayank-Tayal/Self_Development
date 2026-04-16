122396f218a5aff1431998f9c4d5baf1785d43ca
unknown <tayalmayank60@gmail.com>
2026-04-16 16:41:59 +0530

    Done with FormView

--- Django/feedback/reviews/views.py
+++ Django/feedback/reviews/views.py
@@ -3,29 +3,21 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView  
 from .models import Review
 from .forms import ReviewForm
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
     
-        return render(request,"reviews/review.html",{
-            "form": form
-        })
-    
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request,"reviews/review.html",{
-            "form": form
-        })
 
 
 
@@ -43,10 +35,11 @@ class ReviewsListView(ListView):
     model = Review
     context_object_name = "reviews"
     
-    def get_queryset(self):
-        base_query = super().get_queryset()
-        data = base_query.filter(rating__gt=3)
-        return data
+    # if we want specified data to display
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=3)
+    #     return data
     
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
