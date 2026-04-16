082ed4887824c15b42ecbcebe21922e7fca443c7
unknown <tayalmayank60@gmail.com>
2026-04-16 21:00:37 +0530

    Using models for storing files

--- Django/feedback/profiles/views.py
+++ Django/feedback/profiles/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
+from .models import UserProfile
 # Create your views here.
-
-def store_file(file):
-    with open("temp/image.jpeg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
@@ -20,7 +16,8 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
         
         return render(request, "profiles/create_profile.html",{
